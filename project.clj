813455129f5d25d80a0b41f47f95fdb622942efb
Jon Eisen <jon@joneisen.me>
2016-10-28 13:42:27 -0600

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kafka-connect-s3-sink "0.1.2-SNAPSHOT"
+(defproject kafka-connect-s3-sink "0.1.2"
   :description "Kafka Connector Sink to push records to s3"
   :url "https://github.com/yanatan16/kafka-connect-s3-sink"
   :license {:name "MIT"
